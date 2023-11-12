2b2e5f9749e47afebec354c14ce40e4c6f785344
shirow ozawa <striderkein@users.noreply.github.com>
2023-11-12 20:57:32 +0900

    style:delete unused import

--- src/components/OrderInfo.tsx
+++ src/components/OrderInfo.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 import '../styles/style.css';
