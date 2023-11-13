1e8f7308b114c34d5ccaa8516fadba660452bf79
shirow ozawa <striderkein@users.noreply.github.com>
2023-11-13 23:24:16 +0900

    style:delete unused import, comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/style.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import UserHome from './components/UserHome';
 import CustomerInfo from './components/CustomerInfo';
 import OrderInfo from './components/OrderInfo';
@@ -7,8 +7,6 @@ import OrderInfo from './components/OrderInfo';
 export default function App() {
   return (
     <div className="App">
-      {/* <Link to="/">HOME</Link> */}
-      {/* <br /> */}
       <Routes>
         <Route path="/" element={<UserHome />} />
         <Route path="/customer-info/:customerId" element={<CustomerInfo />} />
